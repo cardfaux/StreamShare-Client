9aa88175acc8703568c03593e43f3c4506da94e3
James Hagood <cardfaux@tutanota.com>
2020-01-28 20:19:37 -0600

    added redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-	return (
-		<div>
-			<h1>StreamCreate</h1>
-		</div>
-	);
-};
+class StreamCreate extends React.Component {
+	renderInput(formProps) {
+		return (
+			<input
+				onChange={formProps.input.onChange}
+				value={formProps.input.value}
+			/>
+		);
+	}
 
-export default StreamCreate;
+	render() {
+		return (
+			<div>
+				<form>
+					<Field name='title' component={this.renderInput} />
+					<Field name='description' component={this.renderInput} />
+				</form>
+			</div>
+		);
+	}
+}
+
+export default reduxForm({
+	form: 'streamCreate'
+})(StreamCreate);
